2f321bc290fd19dd195b7779ed439d173f30a24c
Alex Arias <alex.arias.jorym@gmail.com>
2023-02-23 15:38:59 +0800

    feat: create Book and Borrower structs

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+#define SIZE 54
+
+typedef struct
+{
+    int book_id;
+    char title[SIZE], author[SIZE];
+    bool available;
+} Book;
+
+typedef struct
+{
+    int borrower_id;
+    char name[SIZE], borrowed_books[100];
+} Borrower;
+
 int main() {
 
 
